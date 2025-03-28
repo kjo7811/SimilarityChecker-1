ae77efe35dd3ea4fedf7625307103f391b040225
kjo7811 <kjo7811@naver.com>
2025-03-28 10:09:55 +0900

    charCountChecker() added

--- similarity.cpp
+++ similarity.cpp
@@ -3,4 +3,15 @@
 
 struct similarityChecker {
  public:
+  double charCountChecker(std::string a, std::string b) {
+    int aLen = a.length();
+    int bLen = b.length();
+    if (aLen < bLen) {
+      int tmp = aLen;
+      aLen = bLen;
+      bLen = tmp;
+    }
+    if (aLen >= bLen * 2) return 0;
+    return (1 - ((double(aLen) - double(bLen)) / bLen)) * 60;
+  }
 };
--- similarity_test.cpp
+++ similarity_test.cpp
@@ -11,7 +11,32 @@ class CheckerFixture : public testing::Test {
   similarityChecker checker;
 };
 
-TEST_F(CheckerFixture, Test1) {
+TEST_F(CheckerFixture, CountCheckerTest1) {
+  double expect = checker.charCountChecker("ASD", "DSA");
+  EXPECT_EQ(expect, 60);
+}
+
+TEST_F(CheckerFixture, CountCheckerTest2) {
+  double expect = checker.charCountChecker("A", "BB");
+  EXPECT_EQ(expect, 0);
+}
+TEST_F(CheckerFixture, CountCheckerTest3) {
+  double expect = checker.charCountChecker("AAABB", "BAA");
+  EXPECT_EQ(expect, (1 - double(5 - 3) / 3) * 60);
+}
+TEST_F(CheckerFixture, CountCheckerTest4) {
+  double expect = checker.charCountChecker("AA", "AAE");
+  EXPECT_EQ(expect, 30);
+}
+
+TEST_F(CheckerFixture, CountCheckerTest5) {
+  double expect = checker.charCountChecker("AAAAAAAAAA", "AAAAAAA");
+  EXPECT_EQ(expect, (1 - double(10 - 7) / 7) * 60);
+}
+
+TEST_F(CheckerFixture, CountCheckerTest6) {
+  double expect = checker.charCountChecker("AAAAA", "AA");
+  EXPECT_EQ(expect, 0);
 }
 
 using namespace testing;
